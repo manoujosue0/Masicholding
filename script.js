593f7b67a4a40ebfa40c19bec1a5a3cb219bfadf
manoujosue0 <manoujosue0@gmail.com>
2025-04-21 23:40:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -46,3 +46,15 @@ if (window.innerWidth <= 768) {
     }
   });
 }
+  
+<script>
+  document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+        target.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+</script>
